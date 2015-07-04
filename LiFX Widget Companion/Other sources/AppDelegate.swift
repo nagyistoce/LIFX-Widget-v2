fbec38fc6547833acba505902ab51d83de51d0b2
Maxime de Chalendar <maxime.dechalendar@me.com>
2015-07-04 09:33:33 +0200

    Replaced the view controller status bar appearance by the application status bar appearance

--- LiFX Widget Companion/Other sources/AppDelegate.swift
+++ LiFX Widget Companion/Other sources/AppDelegate.swift
@@ -10,16 +10,17 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
+    var window: UIWindow?
 
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
-    themeApplication()
-    return true
-  }
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        themeApplication(application)
+        return true
+    }
 
-    private func themeApplication() {
+    private func themeApplication(application: UIApplication) {
+        application.statusBarStyle = .LightContent
+
         let mainColor = UIColor(red: 35/255, green: 178/255, blue: 184/255, alpha: 1)
-
         UIView.appearance().tintColor = mainColor
 
         SVProgressHUD.setForegroundColor(mainColor)
