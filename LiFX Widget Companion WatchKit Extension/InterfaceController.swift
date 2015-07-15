45dd9f1ea68f4be8626d5eeb7d8f813837768aa5
Maxime de Chalendar <maxime.dechalendar@me.com>
2015-07-15 18:48:33 +0200

    Cleaned up the Watchkit app

---
+++ LiFX Widget Companion WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,13 @@
+//
+//  InterfaceController.swift
+//  LiFX Widget
+//
+//  Created by Maxime de Chalendar on 15/07/2015.
+//  Copyright (c) 2015 Maxime de Chalendar. All rights reserved.
+//
+
+import WatchKit
+
+class InterfaceController: WKInterfaceController {
+
+}
