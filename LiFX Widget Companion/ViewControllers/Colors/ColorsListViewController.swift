20a80292ccd2d638ba92a9ed8873a3733bdf1fda
Maxime de Chalendar <maxime.dechalendar@me.com>
2015-07-18 12:38:02 +0200

    Changed the height of the colour cells, and pinned the view to the table header view when you scroll.

--- LiFX Widget Companion/ViewControllers/Colors/ColorsListViewController.swift
+++ LiFX Widget Companion/ViewControllers/Colors/ColorsListViewController.swift
@@ -57,6 +57,50 @@ extension ColorsListViewController{
     
 }
 
+// UIScrollViewDelegate
+extension ColorsListViewController {
+    
+    override func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView === tableView && decelerate == false {
+            pinTableViewToHeader()
+        }
+    }
+    
+    override func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        if scrollView === tableView {
+            pinTableViewToHeader()
+        }
+    }
+    
+    override func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        if scrollView === tableView {
+            let currentYPosition = tableView.contentOffset.y + tableView.contentInset.top
+            let targetYPosition = targetContentOffset.memory.y + tableView.contentInset.top
+            let headerHeight = CGRectGetHeight(tableView.tableHeaderView!.bounds)
+            if currentYPosition > headerHeight && targetYPosition < headerHeight {
+                targetContentOffset.memory.y = headerHeight - tableView.contentInset.top
+            }
+        }
+    }
+    
+    private func pinTableViewToHeader() {
+        var contentOffset = tableView.contentOffset
+        let scrollPosition = contentOffset.y + tableView.contentInset.top
+        let headerHeight = CGRectGetHeight(tableView.tableHeaderView!.bounds)
+        if scrollPosition < headerHeight / 2 {
+            contentOffset.y = 0
+        }
+        else if scrollPosition < headerHeight {
+            contentOffset.y = headerHeight
+        }
+        else {
+            return ;
+        }
+        contentOffset.y -= tableView.contentInset.top
+        tableView.setContentOffset(contentOffset, animated: true)
+    }
+}
+
 // ColorPickerViewController
 extension ColorsListViewController {
 
