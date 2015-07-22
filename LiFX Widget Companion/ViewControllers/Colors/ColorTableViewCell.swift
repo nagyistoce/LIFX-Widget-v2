d58c90668ee6f5adb53b92053f929d9615790536
Maxime de Chalendar <maxime.dechalendar@me.com>
2015-07-22 19:46:39 +0200

    Added a separator on the color view controller

--- LiFX Widget Companion/ViewControllers/Colors/ColorTableViewCell.swift
+++ LiFX Widget Companion/ViewControllers/Colors/ColorTableViewCell.swift
@@ -14,7 +14,16 @@ class ColorTableViewCell: UITableViewCell {
 
 // Configuration fuctions
 extension ColorTableViewCell {
-   
+    
+    override func layoutSubviews() {
+        // http://stackoverflow.com/questions/25770119/ios-8-uitableview-separator-inset-0-not-working
+        self.layoutMargins = UIEdgeInsetsZero;
+        self.preservesSuperviewLayoutMargins = false;
+
+        super.layoutSubviews()
+    }
+    
+    
     func configureWithColor(color: UIColor) {
         backgroundColor = color
     }
